fe091a610d6b7054eb3fb6b0bc464375134d9a68
Tiffany Yang <teefyang@Tiffanys-MacBook-Pro.local>
2020-06-07 23:53:04 -0700

    creates profile route for create and update

--- server/apis/profile.js
+++ server/apis/profile.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const Profile = require("../models/profileModel");
 const auth = require("../apis/auth");
+const { check, validationResult } = require("express-validator/check");
 
-//@route GET /app/home/me
+//@route GET /app/home
 //@desc GET current users's profile
 //@access Private
 router.get("/", auth, async (req, res) => {
@@ -19,7 +20,67 @@ router.get("/", auth, async (req, res) => {
   } catch (err) {
     res.status(500).send("Server error");
   }
-  //we want to get
 });
+
+//@route POST /app/home
+//@desc CREATE/UPDATE current users's profile
+//@access Private
+
+router.post(
+  "/",
+  [
+    auth,
+    check("diet", "diet is required").not().isEmpty(),
+    check("recommendedRecipes", "Recommended recipes are required")
+      .not()
+      .isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      //init profileFields
+      const profileFields = {};
+      const {
+        user,
+        myTop3Recipes,
+        newsfeed,
+        diet,
+        recommendedRecipes,
+      } = req.body;
+
+      let { userId } = req.user;
+      profileFields.user = userId;
+      if (myTop3Recipes) {
+        profileFields.myTop3Recipes = myTop3Recipes
+          .split(",")
+          .map((each) => each.trim());
+      }
+      if (newsfeed) profileFields.newsfeed = newsfeed;
+      if (diet) profileFields.diet = diet;
+      if (recommendedRecipes)
+        profileFields.recommendedRecipes = recommendedRecipes;
+      //check to see if profile exists:
+      let profile = await Profile.findOne({ user: userId });
+      if (profile) {
+        await Profile.findOneAndUpdate(
+          { user: userId },
+          { $set: profileFields },
+          { new: true }
+        );
+        res.json({ update: profile });
+      } else {
+        profile = new Profile(profileFields);
+        await profile.save();
+        res.json(profile);
+        //create profile
+      }
+    } catch (err) {
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
--- server/models/profileModel.js
+++ server/models/profileModel.js
@@ -6,26 +6,31 @@ const profileSchema = new Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "user",
+    required: true,
   },
   myTop3Recipes: {
-    type: [mongoose.Schema.Types.ObjectId],
-    ref: "Recipe",
+    // type: [mongoose.Schema.Types.ObjectId],
+    // ref: "Recipe",
+    type: [String],
   },
   newsfeed: {
     author: {
       type: String,
     },
     top3Recieps: {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "Recipe",
+      // type: [mongoose.Schema.Types.ObjectId],
+      // ref: "Recipe",
+      type: [String],
     },
   },
   diet: {
     type: [String],
   },
   recommendedRecipes: {
-    type: [mongoose.Schema.Types.ObjectId],
-    ref: "Recipe",
+    // type: [mongoose.Schema.Types.ObjectId],
+    // ref: "Recipe",
+    type: [String],
+    required: true,
   },
 });
 
